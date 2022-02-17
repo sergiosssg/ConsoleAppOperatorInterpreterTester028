ac36c23afb7318bc675f0bd73c279061eff9dbe8
sergiosssg <sergiosssg@gmail.com>
2022-02-17 09:57:32 +0200

    Добавьте файлы проекта.

---
+++ OperatorComparisionBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppOperatorInterpreterTester028
+{
+
+    enum OperatorSignComparision
+    {
+        _EQ_, _NE_, _GT_, _LT_, _GE_, _LE_, _BETWEEN_STRICT_, _BETWEEN_STRICT_LEFT_, _BETWEEN_STRICT_RIGHT_, _BETWEEN_NO_STRICT_, _REGEX_
+    }
+
+    enum OperatorSignLogic
+    {
+        _AND_, _OR_, _NOT_, _NIL_
+    }
+
+    interface IOperatorPredicateForComparision<T>
+    {
+
+    }
+
+
+    interface IOperatorForComparision<T>
+    {
+
+    }
+
+
+}
